26cf9888d61a312d39cf6e7cb7e89c7ae69edd8a
crazygmr101 <bonbonrtt@gmail.com>
2022-01-24 02:57:16 -0500

    Add nox, and untested file SCWEEE

---
+++ noxfile.py
@@ -0,0 +1,21 @@
+import nox
+
+TARGETS = [
+    "bot/",
+    "database/",
+    "dashboard/",
+    "utils.py/",
+    "main.py"
+]
+
+
+@nox.session
+def lint(session):
+    session.install("flake8")
+    session.run("flake8", ".")
+
+
+@nox.session
+def black(session):
+    session.install("black")
+    session.run("black", *TARGETS)
--- utils/config.py
+++ utils/config.py
@@ -1,5 +1,6 @@
 import yamale
 
+
 def validate(config_path: str, schema_path: str):
     yamale.validate(yamale.make_schema(schema_path), yamale.make_data(config_path))
 

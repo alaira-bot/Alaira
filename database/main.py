39649664d8cfa7bc118f257f63a68776d297db5a
crazygmr101 <bonbonrtt@gmail.com>
2022-01-24 03:23:03 -0500

    Add database communicator

--- database/main.py
+++ database/main.py
@@ -1,2 +1,14 @@
+from flask import Flask
+import time
+
+app = Flask("alaira-database")
+
+@app.route("/kill")
+def kill_route():
+    print("Killing database")
+    time.sleep(5)  # TODO
+    print("Database killed")
+    return "", 204
+
 def run():  # started from the main file - not invoked manually
-    pass
+    app.run()
--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import multiprocessing
 import bot.main as bot
 import dashboard.main as dashboard
 import database.main as database
+from database.communicator import DatabaseCommunicator
 import utils.config as cfg
 
 cfg.validate("config.yaml", "schema/config_schema.yaml")
@@ -9,8 +10,12 @@ cfg.validate("config.yaml", "schema/config_schema.yaml")
 bot_proc = multiprocessing.Process(target=bot.run)
 dashboard_proc = multiprocessing.Process(target=dashboard.run)
 database_proc = multiprocessing.Process(target=database.run)
+database_communicator = DatabaseCommunicator(5000)
 
 bot_proc.start()
 dashboard_proc.start()
 database_proc.start()
 bot_proc.join()
+database_communicator.kill()
+dashboard_proc.kill()
+database_proc.kill()

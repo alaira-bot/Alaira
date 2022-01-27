1b2fe3e17036543c49c17e9838b004ae8727107f
crazygmr101 <bonbonrtt@gmail.com>
2022-01-27 02:51:09 -0500

    Add alembic migrations

---
+++ alembic/versions/c097fdea31a0_initial_revision.py
@@ -0,0 +1,24 @@
+"""Initial revision
+
+Revision ID: c097fdea31a0
+Revises: 
+Create Date: 2022-01-27 02:41:51.765048
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'c097fdea31a0'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
---
+++ alembic/versions/cee714b63a7e_add_prefixes.py
@@ -0,0 +1,25 @@
+"""Add prefixes
+
+Revision ID: cee714b63a7e
+Revises: c097fdea31a0
+Create Date: 2022-01-27 02:46:59.969030
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'cee714b63a7e'
+down_revision = 'c097fdea31a0'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    op.create_table("guild_configs", sa.Column("guild", sa.Integer, primary_key=True),
+                    sa.Column("prefix", sa.String, nullable=False, default="!"))
+
+
+def downgrade():
+    op.drop_table("guild_configs")
